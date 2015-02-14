cf83fb1bff5082f270b420b563f359a3d5de6b0b
snarfed <snarfed@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-14 02:28:24 +0000

    support many more micropub params
    git-svn-id: https://plugins.svn.wordpress.org/micropub/trunk@1089590 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- micropub.php
+++ micropub.php
@@ -72,23 +72,19 @@ class Micropub {
     }
 
     // support both action= and operation= parameter names
-    if (!isset($_POST['action']) && isset($_POST['operation'])) {
-      $_POST['action'] = $_POST['operation'];
+    if (!isset($_POST['action'])) {
+      $_POST['action'] = isset($_POST['operation']) ? $_POST['operation']
+        : isset($_POST['url']) ? 'edit' : 'create';
     }
 
-    $args = apply_filters('before_micropub', Micropub::map_params());
+    $args = apply_filters('before_micropub', Micropub::generate_args());
 
     if (!isset($_POST['url']) || $_POST['action'] == 'create') {
       $args['post_status'] = 'publish';
+      kses_remove_filters();  // prevent sanitizing HTML tags in post_content
       $args['ID'] = Micropub::check_error(wp_insert_post($args));
-
-      if (isset($_FILES['photo'])) {
-        require_once(ABSPATH . 'wp-admin/includes/image.php');
-        require_once(ABSPATH . 'wp-admin/includes/file.php');
-        require_once(ABSPATH . 'wp-admin/includes/media.php');
-        Micropub::check_error(media_handle_upload('photo', $args['ID']));
-      }
-
+      kses_init_filters();
+      Micropub::postprocess($args['ID']);
       status_header(201);
       header('Location: ' . get_permalink($args['ID']));
 
@@ -100,11 +96,14 @@ class Micropub {
       }
 
       if ($_POST['action'] == 'edit' || !isset($_POST['action'])) {
+        kses_remove_filters();  // prevent sanitizing HTML tags in post_content
         Micropub::check_error(wp_update_post($args));
-        status_header(204);
+        kses_init_filters();
+        Micropub::postprocess($args['ID']);
+        status_header(200);
       } elseif ($_POST['action'] == 'delete') {
         Micropub::check_error(wp_trash_post($args['ID']));
-        status_header(204);
+        status_header(200);
       // TODO: figure out how to make url_to_postid() support posts in trash
       // here's one way:
       // https://gist.github.com/peterwilsoncc/bb40e52cae7faa0e6efc
@@ -113,7 +112,7 @@ class Micropub {
       //     'ID'           => $args['ID'],
       //     'post_status'  => 'publish',
       //   )));
-      //   status_header(204);
+      //   status_header(200);
       } else {
         status_header(400);
         echo 'unknown action ' . $_POST['action'];
@@ -134,9 +133,7 @@ class Micropub {
     } elseif (isset($_POST['access_token'])) {
       $auth_header = 'Bearer ' . $_POST['access_token'];
     } else {
-      status_header(401);
-      echo 'Missing access token';
-      exit;
+      return Micropub::handle_authorize_error(401, 'missing access token');
     }
 
     $resp = wp_remote_get('https://tokens.indieauth.com/token',
@@ -146,35 +143,43 @@ class Micropub {
     $code = wp_remote_retrieve_response_code($resp);
     $body = wp_remote_retrieve_body($resp);
     if ($code / 100 != 2) {
-      status_header($code);
-      echo 'Invalid access token: ' . $body;
-      exit;
+      return Micropub::handle_authorize_error(
+        $code, 'invalid access token: ' . $body);
     }
 
     parse_str($body, $resp);
     $home = untrailingslashit(home_url());
-    if ($home != 'http://localhost' &&
-        $home != untrailingslashit($resp['me'])) {
-      status_header(401);
-      echo 'access token domain ' . $resp['me'] . " doesn't match " . $url;
-      exit;
+    $me = untrailingslashit($resp['me']);
+    if ($home != $me) {
+      return Micropub::handle_authorize_error(
+        401, 'access token URL ' . $me . " doesn't match " . $home);
     } else if (!isset($resp['scope']) ||
                !in_array('post', explode(' ', $resp['scope']))) {
-      status_header(403);
-      echo "access token is missing post scope; got " . $resp['scope'];
-      exit;
+      return Micropub::handle_authorize_error(
+        403, 'access token is missing post scope; got ' . $resp['scope']);
     }
   }
 
+  private static function handle_authorize_error($code, $msg) {
+    $home = untrailingslashit(home_url());
+    if ($home == 'http://localhost') {
+        echo 'WARNING: ' . $code . ' ' . $msg .
+          ". Allowing only because this is localhost.\n";
+        return;
+    }
+    status_header($code);
+    echo $msg;
+    exit;
+  }
+
   /**
-   * Map Micropub parameters to WordPress wp_insert_post() args.
+   * Generate args for WordPress wp_insert_post() and friends.
    */
-  private static function map_params() {
+  private static function generate_args() {
     // these can be passed through untouched
     $mp_to_wp = array(
       'slug'     => 'post_name',
       'name'     => 'post_title',
-      'content'  => 'post_content',
       'summary'  => 'post_excerpt'
     );
 
@@ -185,10 +190,6 @@ class Micropub {
       }
     }
 
-    if (isset($_FILES['photo'])) {
-      $args['post_content'] .= "\n\n[gallery size=full columns=1]";
-    }      
-
     // these are transformed or looked up
     if (isset($_POST['url'])) {
       $args['ID'] = url_to_postid($_POST['url']);
@@ -198,13 +199,135 @@ class Micropub {
       $args['post_date'] = iso8601_to_datetime($_POST['published']);
     }
 
+    // Map micropub categories to WordPress categories if they exist, otherwise
+    // to WordPress tags.
+    if (isset($_POST['category'])) {
+      foreach ($_POST['category'] as $mp_cat) {
+        $wp_cat = get_category_by_slug($mp_cat);
+        if ($wp_cat) {
+          $args['post_category'][] = $wp_cat->term_id;
+        } else {
+          $args['tags_input'][] = $mp_cat;
+        }
+      }
+    }
+
+    $args['post_content'] = Micropub::generate_post_content();
     return $args;
   }
+
+  /**
+   * Generates and returns a post_content string suitable for wp_insert_post()
+   * and friends.
+   */
+  private static function generate_post_content() {
+    // interactions
+    foreach (array('like', 'repost', 'in-reply-to') as $cls) {
+      $val = isset($_POST[$cls]) ? $_POST[$cls]
+             : (isset($_POST[$cls . '-of']) ? $_POST[$cls . '-of']
+             : NULL);
+      if ($val) {
+        if ($cls != 'in-reply-to') {
+          $cls .= 's';
+        }
+        $lines[] = '<p>' . ucfirst(str_replace('-', ' ', $cls)) .
+          ' <a class="u-' . $cls . ' href="' . $val . '">' . $val . '</a>.</p>';
+      }
+    }
+
+    if (isset($_POST['rsvp'])) {
+      $lines[] = '<p>RSVPs <data class="p-rsvp" value="' . $_POST['rsvp'] .
+        '">' . $_POST['rsvp'] . '</data>.</p>';
+    }
+
+    // content, event
+    if (isset($_POST['content'])) {
+      $lines[] = '<div class="e-content">';
+      $lines[] = $_POST['content'];
+      if (isset($_POST['h']) && $_POST['h'] == 'event') {
+        $lines[] = Micropub::generate_event();
+      }
+      $lines[] = '</div>';
+    }
+
+    // TODO: generate my own markup so i can include u-photo
+    if (isset($_FILES['photo'])) {
+      $lines[] = "\n[gallery size=full columns=1]";
+    }
+
+    return implode("\n", $lines);
+  }
+
+  /**
+   * Generates and returns a string h-event.
+   */
+  private static function generate_event() {
+    $lines[] = '<div class="h-event">';
+
+    if (isset($_POST['name'])) {
+      $lines[] = '<h1 class="p-name">' . $_POST['name'] . '</h1>';
+    }
+
+    $lines[] = '<p>';
+    $times = array();
+    foreach (array('start', 'end') as $cls) {
+      if (isset($_POST[$cls])) {
+        $datetime = iso8601_to_datetime($_POST[$cls]);
+        $times[] = '<time class="dt-' . $cls . '" datetime="' . $_POST[$cls] .
+          '">' . $datetime . '</time>';
+      }
+    }
+    $lines[] = implode("\nto\n", $times);
+
+    if (isset($_POST['location']) && substr($_POST['location'], 0, 4) != 'geo:') {
+      $lines[] = 'at <a class="p-location" href="' . $_POST['location'] . '">' .
+        $_POST['location'] . '</a>';
+    }
+
+    end($lines);
+    $lines[key($lines)] .= '.';
+    $lines[] = '</p>';
+
+    if (isset($_POST['summary'])) {
+      $lines[] = '<p class="p-summary">' . urldecode($_POST['summary']) . '</p>';
+    }
+  
+    if (isset($_POST['description'])) {
+      $lines[] = '<p class="p-description">' . urldecode($_POST['description']) . '</p>';
+    }
+
+    $lines[] = '</div>';
+    return implode("\n", $lines);
+  }
+
+  /**
+   * Postprocesses a post that has been created or updated. Useful for changes
+   * that require the post id, e.g. uploading media and adding post metadata.
+   */
+  private static function postprocess($post_id) {
+    if (isset($_FILES['photo'])) {
+      require_once(ABSPATH . 'wp-admin/includes/image.php');
+      require_once(ABSPATH . 'wp-admin/includes/file.php');
+      require_once(ABSPATH . 'wp-admin/includes/media.php');
+      Micropub::check_error(media_handle_upload('photo', $post_id));
+    }
+
+    if (isset($_POST['location']) && substr($_POST['location'], 0, 4) == 'geo:') {
+      // Geo URI format:
+      // http://en.wikipedia.org/wiki/Geo_URI#Example
+      // https://indiewebcamp.com/micropub##location
+      $geo = str_replace('geo:', '', urldecode($_POST['location']));
+      $geo = explode(';', explode(':', $geo)[0])[0];
+      $coords = explode(',', $geo);
+      update_post_meta($post_id, 'geo_latitude', trim($coords[0]));
+      update_post_meta($post_id, 'geo_longitude', trim($coords[1]));
+    }
+}
 
   private static function check_error($result) {
     if (!$result) {
       status_header(500);
-      echo 'Unknown WordPress error';
+      echo 'Unknown WordPress error: ' . $result;
       exit;
     } else if (is_wp_error($result)) {
       status_header(500);
--- readme.txt
+++ readme.txt
@@ -19,18 +19,31 @@ A [Micropub](http://micropub.net/) server plugin. From [micropub.net](http://mic
 Supports create, update, and delete, but not undelete. Supports these
 Micropub properties:
 
+* `category` maps to WordPress category if it exists, otherwise to WordPress tag
 * `content`
+* `description`
+* `end`
+* `h`=entry and `h`=event
+* `in-reply-to`
+* `like-of`/`like`
+* `location` is stored in [WordPress standard geodata format](http://codex.wordpress.org/Geodata)
 * `name`
-* `summary`
-* `slug`
-* `url`
-* `published`
 * `photo`
+* `published`
+* `repost-of`/`repost`
+* `rsvp`
+* `slug`
+* `start`
+* `summary`
+* `url`
 
 Adds one WordPress filter, `before_micropub($wp_args)`, and one hook,
 `after_micropub($post_id)`.
 
-Delegates token handling to [tokens.indieauth.com](https://tokens.indieauth.com/).
+Delegates token handling to
+[tokens.indieauth.com](https://tokens.indieauth.com/). For ease of development,
+if the WordPress site is running on `localhost`, it logs a warning if the access
+token is missing or invalid and still allows the request.
 
 Development happens at http://github.com/snarfed/wordpress-micropub . Feedback
 and pull requests are welcome!
